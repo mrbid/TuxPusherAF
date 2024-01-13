0c8bad256e9564f4af08d6e2184cf98d2cbec8ca
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2024-01-13 00:14:16 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1313,20 +1313,20 @@ void main_loop()
     // cursor icon switching based on the mouse position. 
     // have a cross while over the playing field.
 #ifndef BUILD_GLFW
-    // cursor
-    switch (cursor_state) 
+    if(cursor_state == 0 && mx < touch_margin-1.f)
     {
-        case 0:
-            if (mx <= ww-touch_margin+1.f && mx >= touch_margin-1.f)
-                break;
-            SDL_SetCursor(beam_cursor);
-            cursor_state = 1;
-            break;
-        case 1:
-            if (mx > touch_margin && mx < ww-touch_margin) 
-                SDL_SetCursor(cross_cursor);
-                cursor_state = 0;
-            break;
+        SDL_SetCursor(beam_cursor);
+        cursor_state = 1;
+    }
+    else if(cursor_state == 0 && mx > ww-touch_margin+1.f)
+    {
+        SDL_SetCursor(beam_cursor);
+        cursor_state = 1;
+    }
+    else if(cursor_state == 1 && mx > touch_margin && mx < ww-touch_margin)
+    {
+        SDL_SetCursor(cross_cursor);
+        cursor_state = 0;
     }
 #endif
     
